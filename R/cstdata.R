9a84e566f5bc8ba003247feba8096aca719ee854
williamstravis <travis.williams@colorado.edu>
2019-10-04 15:06:39 -0600

    Creating a branch to incoporate options for storage location

--- R/cstdata.R
+++ R/cstdata.R
@@ -16,8 +16,8 @@
 #' 
 #' Production Notes:
 #'
-#' - The use of reticulate may enable us to use Zarr arrays, which are accessible
-#'   directly from an s3 bucket.
+#' - The use of reticulate may enable us to use Zarr arrays, which are
+#'   accessible directly from an s3 bucket.
 #'
 #' - This currently saves everything to disk first, which can be a problem. The
 #'   alternative is to save each file to a tempfile and overwrite. We'll have to
@@ -25,19 +25,43 @@
 #'   ability to save locally, and for small parks this is fine, so let's an
 #'   build in an option to save locally and to the s3 bucket.
 #' 
-#' @param parkname The name of the U.S. National Park for which download data
-#' (character), e.g., "Yellowstone National Park"
-#' @param start_year The first year of the desired period (integer).
-#' @param end_year The last year of the desired period (integer).
+#' @param parkname The name of the U.S. National Park for which to download data
+#' , e.g., "Yellowstone National Park". (character)
+#' @param start_year The first year of the desired period. (integer)
+#' @param end_year The last year of the desired period. (integer)
+#' @param store_locally If `TRUE` this function will store the results in a
+#' local directory as NetCDF files. This options may be set to `FALSE` to save
+#' disk space, but the `store_remotely` option must be set to `TRUE` in this
+#' case. (boolean)
+#' @param local_dir The local directory in which to save files if
+#' `store_locally` is set to `TRUE`. (character)
+#' @param store_remotely If `TRUE` this function will store the results in an
+#' Amazon Web Services S3 bucket. This will require the user to store an S3
+#' credentials file on their local machine. (boolean)
+#' @param aws_config_dir The local directory in which to save the configuration
+#' file needed for storing data in an AWS S3 bucket. If the file is not yet
+#' present in this directory, the user will be prompted for the information
+#' needed to build the file. (character)
+#'
 #' @importFrom methods new
 
+
 cstdata <- function(parkname="Acadia National Park", start_year = 1950,
-                    end_year = 2099){
-  # Initialize AWS access
-  creds <- readRDS("~/.aws/credentials.RDS")
-  Sys.setenv("AWS_ACCESS_KEY_ID" = creds['key'],
-             "AWS_SECRET_ACCESS_KEY" = creds['skey'],
-             "AWS_DEFAULT_REGION" = creds['region'])
+                    end_year = 2099, store_locally = TRUE,
+                    local_dir = "cstdata", store_remotely = TRUE,
+                    aws_config_dir = "~/.aws"){
+
+  # Make sure user is choosing to store somewhere
+  if (store_locally == FALSE & store_remotely == FALSE) {
+    msg <- "Choose to store either locally, remotely, or both"
+    stop(message)
+  }
+
+  # Set up AWS access
+  bucket = "na"
+  if (store_remotely == TRUE) {
+    bucket <- config_aws(aws_config_dir)
+  }
 
   # Make sure we have the national park shapefile
   get_park_boundaries()
@@ -81,7 +105,7 @@ cstdata <- function(parkname="Acadia National Park", start_year = 1950,
   raster::extent(r) <- raster::extent(aoi)
   r <- raster::rasterize(aoi, r, 'UNIT_CODE')  # <------------------------------ Not generalizable
   mask_grid <- r * 0 + 1
-  mask_mat <- as.matrix(mask_grid)
+  mask_mat <- as(mask_grid, "matrix")
 
   # Get some time information
   ntime_hist <- grid$ntime_hist
@@ -132,13 +156,39 @@ cstdata <- function(parkname="Acadia National Park", start_year = 1950,
 
   # With parallelization
   for (gq in gqueries) {
-      t <- foreach::foreach(i=1:length(gq)) %dopar% {  # <---------------------- Build note "no visible binding for global variable ‘i’"
+      t <- foreach::foreach(i=1:length(gq)) %dopar% {  # <---------------------- Build note: "no visible binding for global variable ‘i’"  
             retrieve_subset(gq[[i]], start_year, end_year, dst_folder, parkname,
                             aoilats, aoilons, mask_mat, latmin, latmax, lonmin,
-                            lonmax, resolution)
+                            lonmax, resolution, bucket, local_dir,
+                            store_locally = TRUE, store_remotely = TRUE)
       }
   }
   parallel::stopCluster(cl)
+}
+
+
+config_aws <- function(aws_config_dir){
+  aws_config_file <- file.path(aws_config_dir, "cstdata_config.RDS")
+
+  # Build configuration file if needed
+  if (!file.exists(aws_config_file)) {
+    print("Build AWS Configuration File\n")
+    bucket <- readline("s3 bucket name: ")
+    key <- readline("aws key: ")
+    skey <- readline("aws secret key: ")
+    region <- readline("aws region: ")
+    creds <- c("bucket" = bucket, "key" = key, "skey" = skey, "region" = region)
+    saveRDS(creds, aws_config_file)
+  }
+
+  # Initialize AWS access
+  creds <- readRDS(aws_config_file)
+  Sys.setenv("AWS_ACCESS_KEY_ID" = creds['key'],
+             "AWS_SECRET_ACCESS_KEY" = creds['skey'],
+             "AWS_DEFAULT_REGION" = creds['region'])
+  bucket_name <- creds['bucket']
+
+  return(bucket_name)
 }
 
 
@@ -170,6 +220,7 @@ filter_years <- function(start_year = 1950, end_year = 2099,
   return(c(t1, t2))
 }
 
+
 get_park_boundaries <- function(dir = "data") {
   # Create directory if not present
   prefix <- file.path(dir, "shapefiles")
@@ -188,13 +239,16 @@ get_park_boundaries <- function(dir = "data") {
 
 retrieve_subset <- function(query, start_year, end_year, dst_folder, parkname,
                             aoilats, aoilons, mask_mat, latmin, latmax, lonmin,
-                            lonmax, resolution){
+                            lonmax, resolution, bucket, local_dir,
+                            store_locally = TRUE, store_remotely = TRUE){
   xr <- reticulate::import("xarray")
 
-  # Get the local destination file
-  file_name <- query[[2]]
-  dst <- file.path(dst_folder, file_name)
+  # Get the destination file
   location <- gsub(" ", "_", tolower(parkname))
+  local_park_dir <- file.path(local_dir, location)
+  dst_folder <- ifelse(store_locally == TRUE, local_park_dir, tempfile()) 
+  file_name <- ifelse(store_locally == TRUE, query[[2]], tempfile()) 
+  dst <- file.path(dst_folder, file_name)
 
   if (!file.exists(dst)) {
     # Get the combined historical and modeled url query
@@ -296,11 +350,12 @@ retrieve_subset <- function(query, start_year, end_year, dst_folder, parkname,
     print(end - start)
 
     # Put the file in the bucket
-    print("Saving file to cloud...")
-    bucket_name <- "cstdata-test"
-    object <- file.path(location, file_name)
-    aws.s3::put_folder(location, bucket_name)
-    aws.s3::put_object(file = dst, object = object, bucket = bucket_name)
+    if (store_remotely == TRUE) {
+      print("Saving file to cloud...")
+      object <- file.path(location, file_name)
+      aws.s3::put_folder(location, bucket)
+      aws.s3::put_object(file = dst, object = object, bucket = bucket)
+    }
   }
 }
 
