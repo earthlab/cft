5f400bfe1eccb8f02b28989eeb6c0bd5a449b9cb
williamstravis <travis.williams@colorado.edu>
2019-10-06 15:41:54 -0600

    Modularized and added a new progress bar

--- R/cstdata.R
+++ R/cstdata.R
@@ -19,12 +19,6 @@
 #' - The use of reticulate may enable us to use Zarr arrays, which are
 #'   accessible directly from an s3 bucket.
 #'
-#' - This currently saves everything to disk first, which can be a problem. The
-#'   alternative is to save each file to a tempfile and overwrite. We'll have to
-#'   be careful when parallelizing. However, there has been interest in the
-#'   ability to save locally, and for small parks this is fine, so let's an
-#'   build in an option to save locally and to the s3 bucket.
-#' 
 #' @param parkname The name of the U.S. National Park for which to download data
 #' , e.g., "Yellowstone National Park". (character)
 #' @param start_year The first year of the desired period. (integer)
@@ -37,90 +31,209 @@
 #' `store_locally` is set to `TRUE`. (character)
 #' @param store_remotely If `TRUE` this function will store the results in an
 #' Amazon Web Services S3 bucket. This will require the user to store an S3
-#' credentials file on their local machine. (boolean)
+#' configuration file on their local machine. (boolean)
 #' @param aws_config_dir The local directory in which to save the configuration
 #' file needed for storing data in an AWS S3 bucket. If the file is not yet
 #' present in this directory, the user will be prompted for the information
 #' needed to build the file. (character)
 #'
-#' @importFrom methods new
+#' @importFrom methods new  # <------------------------------------------------- What's this?
 
 
+# Main Function
 cstdata <- function(parkname="Acadia National Park", start_year = 1950,
                     end_year = 2099, store_locally = TRUE,
                     local_dir = "cstdata", store_remotely = TRUE,
                     aws_config_dir = "~/.aws"){
-  '
-  parkname="Theodore Roosevelt National Park"
-  start_year = 1990
-  end_year = 2000
-  store_locally = TRUE
-  local_dir = "cstdata"
-  store_remotely = TRUE
-  aws_config_dir = "~/.aws"
-  '
-  
+
   # Make sure user is choosing to store somewhere
   if (store_locally == FALSE & store_remotely == FALSE) {
     msg <- "Choose to store either locally, remotely, or both"
     stop(msg)
   }
-  
+
+  # If that works, tell them whats happening  <--------------------------------- Tell them installation folder, too
+  print(paste("Retrieving climate data for", parkname))
+
   # Set up AWS access
-  bucket = "na"
   if (store_remotely == TRUE) {
     bucket <- config_aws(aws_config_dir)
+  } else{
+    bucket = "na"
   }
-  
-  # Make sure we have the national park shapefile
-  get_park_boundaries()
-  
-  # Get the boundaries of the chosen national park
-  parks <- rgdal::readOGR("data/shapefiles/nps_boundary.shp")
-  aoi <- parks[grepl(parkname, parks$UNIT_NAME),]
-  
-  # Make sure we a park-specific destination folder  
-  location <- gsub(" ", "_", tolower(aoi$UNIT_NAME))
-  print(paste("Retrieving climate data for", location))
-  
+
+  # Get national park area of interest
+  aoi <- get_park_boundaries(parkname)
+
   # Generate reference objects
-  grid = Grid_Reference()
+  grid_ref = Grid_Reference()
   arg_ref = Argument_Reference()
-  
-  # Get relative index positions to full grid  # <------------------------------ This can be wrapped in a function, but I currently some of the parts generated here.
-  aoi <- sp::spTransform(aoi, grid$crs)
+
+  # Get geographic information about the aoi
+  aoi_info <- get_aoi_info(aoi, grid_ref)
+
+  # Now we can get the historical and model years together
+  gqueries <- get_grouped_queries(aoi, start_year, end_year, arg_ref, grid_ref)
+
+  # Setup parallelization
+  ncores <- parallel::detectCores() / 2
+  `%dopar%` <- foreach::`%dopar%`
+  cl <- parallel::makeCluster(ncores)
+  doParallel::registerDoParallel(cl)
+  parallel::clusterExport(cl, c("retrieve_subset", "filter_years"),
+                          envir = environment())
+
+  # Retrieve Subsets
+  pb <- utils::txtProgressBar(min = 0, max = length(gqueries), style = 3)
+  utils::setTxtProgressBar(pb, 0) 
+  for (g in seq(length(gqueries))) {
+    gq <- gqueries[[g]]
+    t <- foreach::foreach(i=1:length(gq)) %dopar% {  # <------------------------ Build note: "no visible binding for global variable ‘i’"  
+      retrieve_subset(gq[[i]], start_year, end_year, parkname, aoi_info,
+                      bucket, local_dir, store_locally = store_locally,
+                      store_remotely = store_remotely)
+    }
+    utils::setTxtProgressBar(pb, g) 
+  }
+
+  # Done
+  close(pb)
+  parallel::stopCluster(cl)
+}
+
+
+# Sub functions
+config_aws <- function(aws_config_dir){
+  aws_config_file <- file.path(aws_config_dir, "cstdata_config.RDS")
+
+  # Build configuration file if needed
+  if (!file.exists(aws_config_file)) {
+    print("Build AWS Configuration File\n")
+    bucket <- readline("s3 bucket name: ")
+    key <- readline("aws key: ")
+    skey <- readline("aws secret key: ")
+    region <- readline("aws region: ")
+    creds <- c("bucket" = bucket, "key" = key, "skey" = skey, "region" = region)
+    saveRDS(creds, aws_config_file)
+  }
+
+  # Initialize AWS access
+  creds <- readRDS(aws_config_file)
+  Sys.setenv("AWS_ACCESS_KEY_ID" = creds['key'],
+             "AWS_SECRET_ACCESS_KEY" = creds['skey'],
+             "AWS_DEFAULT_REGION" = creds['region'])
+  bucket_name <- creds['bucket']
+
+  # Done.
+  return(bucket_name)
+}
+
+
+filter_years <- function(start_year = 1950, end_year = 2099, 
+                         available_start = 1950, available_end = 2099) {
+  # Turn these into strings, then dates
+  h1 <- as.Date(glue::glue("{available_start}-01-01"))
+  h2 <- as.Date(glue::glue("{available_end}-12-31"))
+  d1 <- as.Date(glue::glue("{start_year}-01-01"))
+  d2 <- as.Date(glue::glue("{end_year}-12-31"))
+
+  # If the end year is before the start year, go ahead and switch them
+  if (d2 < d1) {
+    tmp1 <- d1
+    d1 <- d2
+    d2 <- tmp1
+    rm(tmp1)
+  }
+
+  # Now, if they asked for more than is available, truncate
+  if (d1 < h1) d1 = h1
+  if (d2 > h2) d2 = h2
+
+  # Return the difference between the target and start dates
+  t1 <- as.integer(d1 - h1)
+  t2 <- as.integer(d2 - h1)
+
+  # Return these as a pair
+  return(c(t1, t2))
+}
+
+
+get_aoi_indexes <- function(aoi, grid_ref){
   lonmin <- aoi@bbox[1,1]
   lonmax <- aoi@bbox[1,2]
   latmin <- aoi@bbox[2,1]
   latmax <- aoi@bbox[2,2]
-  lonmindiffs <- abs(grid$lons - lonmin)
-  lonmaxdiffs <- abs(grid$lons - lonmax)
-  latmindiffs <- abs(grid$lats - latmin)
-  latmaxdiffs <- abs(grid$lats - latmax)
+  lonmindiffs <- abs(grid_ref$lons - lonmin)
+  lonmaxdiffs <- abs(grid_ref$lons - lonmax)
+  latmindiffs <- abs(grid_ref$lats - latmin)
+  latmaxdiffs <- abs(grid_ref$lats - latmax)
   x1 <- match(lonmindiffs[lonmindiffs == min(lonmindiffs)], lonmindiffs)
   x2 <- match(lonmaxdiffs[lonmaxdiffs == min(lonmaxdiffs)], lonmaxdiffs)
   y1 <- match(latmindiffs[latmindiffs == min(latmindiffs)], latmindiffs)
   y2 <- match(latmaxdiffs[latmaxdiffs == min(latmaxdiffs)], latmaxdiffs)
-  ny <- (y2 - y1) + 1
-  nx <- (x2 - x1) + 1
-  aoilats <- sapply(1:ny, function(x) latmin + x*grid$resolution)
-  aoilons <- sapply(1:nx, function(x) lonmin + x*grid$resolution)
-  resolution <- grid$resolution
-  
-  # Now create a mask for later
+  index_pos <- list("y1" = y1, "y2" = y2, "x1" = x1, "x2" = x2)
+  return(index_pos)
+}
+
+
+get_aoi_info <- function(aoi, grid_ref) {
+  # Match coordinate systems
+  aoi <- sp::spTransform(aoi, grid_ref$crs)
+
+  # Get relative index positions to full grid
+  index_pos <- get_aoi_indexes(aoi, grid_ref)
+  y1 <- index_pos[["y1"]]
+  y2 <- index_pos[["y2"]]
+  x1 <- index_pos[["x1"]]
+  x2 <- index_pos[["x2"]]
+
+  # Get list of lats and lons
+  res <- grid_ref$resolution
+  ny <- (y2 - y1)
+  nx <- (x2 - x1)
+  latmin <- aoi@bbox[2,1]
+  lonmin <- aoi@bbox[1,1]
+  aoilats <- sapply(0:ny, function(x) latmin + x*res)
+  aoilons <- sapply(0:nx, function(x) lonmin + x*res)
+
+  # Now create a mask as a matrix
   r <- raster::raster(ncols=length(aoilons), nrows=length(aoilats))
   raster::extent(r) <- raster::extent(aoi)
   r <- raster::rasterize(aoi, r, 'UNIT_CODE')  # <------------------------------ Not generalizable
   mask_grid <- r * 0 + 1
   mask_mat <- methods::as(mask_grid, "matrix")
-  
-  # Get some time information
-  ntime_hist <- grid$ntime_hist
-  ntime_model <- grid$ntime_model
-  
-  # Now we can get the historical and model years together
-  urlbase = paste0("http://thredds.northwestknowledge.net:8080/thredds/dodsC/",
-                   "agg_macav2metdata")
+
+  # Package all of this into one object
+  aoi_info <- list("aoilats" = aoilats,
+                   "aoilons" = aoilons,
+                   "mask_mat" = mask_mat, 
+                   "resolution" = res)
+
+  # Done.
+  return(aoi_info)
+}
+
+
+get_grouped_queries <- function(aoi, start_year, end_year, arg_ref, grid_ref){
+  # We are building url queries from this base
+  urlbase = paste0("http://thredds.northwestknowledge.net:8080/thredds/",
+                   "dodsC/agg_macav2metdata")
+
+  # This will be the folder name for this park
+  location <- gsub(" ", "_", tolower(aoi$UNIT_NAME))
+
+  # Get time information from our grid reference
+  ntime_hist <- grid_ref$ntime_hist
+  ntime_model <- grid_ref$ntime_model
+
+  # Get relative index positions to full grid
+  index_pos <- get_aoi_indexes(aoi, grid_ref)
+  y1 <- index_pos[["y1"]]
+  y2 <- index_pos[["y2"]]
+  x1 <- index_pos[["x1"]]
+  x2 <- index_pos[["x2"]]
+
+  # Build a list of vectors with historical and modeled queries and a filename
   queries <- list()
   for (model in arg_ref$models){
     args = arg_ref$get_args(model)
@@ -140,10 +253,12 @@ cstdata <- function(parkname="Acadia National Park", start_year = 1950,
         mattachment <- paste(c(urlbase, param, model, ensemble, scenario,
                                "2006_2099", "CONUS_daily.nc?"), collapse = "_")
         var <- variables[param]
-        hquery <- paste0(var, glue::glue("[{0}:{1}:{ntime_hist}][{y1}:{1}:{y2}]
-                                         [{x1}:{1}:{x2}]#fillmismatch"))
-        mquery <- paste0(var, glue::glue("[{0}:{1}:{ntime_model}][{y1}:{1}:{y2}]
-                                         [{x1}:{1}:{x2}]#fillmismatch"))
+        q1 <- paste0("[{0}:{1}:{ntime_hist}][{y1}:{1}:{y2}][{x1}:{1}:{x2}]",
+                     "#fillmismatch")
+        q2 <- paste0("[{0}:{1}:{ntime_model}][{y1}:{1}:{y2}][{x1}:{1}:{x2}]",
+                     "#fillmismatch")
+        hquery <- paste0(var, glue::glue(q1))
+        mquery <- paste0(var, glue::glue(q2))
         hurl <- paste0(hattachment, hquery)
         murl <- paste0(mattachment, mquery)
         purl <- c(hurl, murl)
@@ -151,89 +266,22 @@ cstdata <- function(parkname="Acadia National Park", start_year = 1950,
       }
     }
   }
-  
-  # If we want to parallelize, we could group the pairs further
+
+  # Group these queries into groups based on the number of physical cpus
   ncores <- parallel::detectCores() / 2
   gqueries <- split(queries, ceiling(seq_along(queries)/ncores))
-  `%dopar%` <- foreach::`%dopar%`
-  cl <- parallel::makeCluster(ncores)
-  doParallel::registerDoParallel(cl)
-  parallel::clusterExport(cl, c("retrieve_subset", "filter_years"),
-                          envir = environment())
-  
-  # With parallelization
-  for (gq in gqueries) {
-    t <- foreach::foreach(i=1:length(gq)) %dopar% {  # <---------------------- Build note: "no visible binding for global variable ‘i’"  
-      retrieve_subset(gq[[i]], start_year, end_year, parkname, aoilats, aoilons,
-                      mask_mat, latmin, latmax, lonmin, lonmax, resolution,
-                      bucket, local_dir, store_locally = store_locally,
-                      store_remotely = store_remotely)
-    }
-  }
-  parallel::stopCluster(cl)
+
+  # Done.
+  return(gqueries)  
 }
 
 
-config_aws <- function(aws_config_dir){
-  aws_config_file <- file.path(aws_config_dir, "cstdata_config.RDS")
-  
-  # Build configuration file if needed
-  if (!file.exists(aws_config_file)) {
-    print("Build AWS Configuration File\n")
-    bucket <- readline("s3 bucket name: ")
-    key <- readline("aws key: ")
-    skey <- readline("aws secret key: ")
-    region <- readline("aws region: ")
-    creds <- c("bucket" = bucket, "key" = key, "skey" = skey, "region" = region)
-    saveRDS(creds, aws_config_file)
-  }
-  
-  # Initialize AWS access
-  creds <- readRDS(aws_config_file)
-  Sys.setenv("AWS_ACCESS_KEY_ID" = creds['key'],
-             "AWS_SECRET_ACCESS_KEY" = creds['skey'],
-             "AWS_DEFAULT_REGION" = creds['region'])
-  bucket_name <- creds['bucket']
-  
-  return(bucket_name)
-}
-
-
-filter_years <- function(start_year = 1950, end_year = 2099, 
-                         available_start = 1950, available_end = 2099) {
-  # Turn these into strings, then dates
-  h1 <- as.Date(glue::glue("{available_start}-01-01"))
-  h2 <- as.Date(glue::glue("{available_end}-12-31"))
-  d1 <- as.Date(glue::glue("{start_year}-01-01"))
-  d2 <- as.Date(glue::glue("{end_year}-12-31"))
-  
-  # If the end year is before the start year, go ahead and switch them
-  if (d2 < d1) {
-    tmp1 <- d1
-    d1 <- d2
-    d2 <- tmp1
-    rm(tmp1)
-  }
-  
-  # Now, if they asked for more than is available, truncate
-  if (d1 < h1) d1 = h1
-  if (d2 > h2) d2 = h2
-  
-  # Return the difference between the target and start dates
-  t1 <- as.integer(d1 - h1)
-  t2 <- as.integer(d2 - h1)
-  
-  # Return these as a pair
-  return(c(t1, t2))
-}
-
-
-get_park_boundaries <- function(dir = "data") {
+get_park_boundaries <- function(parkname, dir = "data") {
   # Create directory if not present
   prefix <- file.path(dir, "shapefiles")
   dir.create(prefix, recursive = TRUE, showWarnings = FALSE)
   nps_boundary <- file.path(prefix, "nps_boundary.shp")
-  
+
   # Download if needed
   if (!file.exists(nps_boundary)){
     file <- file.path(prefix, "nps_boundary.zip")
@@ -241,15 +289,27 @@ get_park_boundaries <- function(dir = "data") {
     utils::download.file(url = url, destfile = file, method = "curl")
     utils::unzip(file, exdir = prefix)
   }
+
+  # Get the boundaries of the chosen national park
+  parks <- rgdal::readOGR(nps_boundary)
+  aoi <- parks[grepl(parkname, parks$UNIT_NAME),]
+
+  return(aoi)
 }
 
 
-retrieve_subset <- function(query, start_year, end_year, parkname, aoilats,
-                            aoilons, mask_mat, latmin, latmax, lonmin, lonmax,
-                            resolution, bucket, local_dir, store_locally = TRUE,
+retrieve_subset <- function(query, start_year, end_year, parkname, aoi_info,
+                            bucket, local_dir, store_locally = TRUE,
                             store_remotely = TRUE){
+  # Load xarray
   xr <- reticulate::import("xarray")
-  
+
+  # Unpack aoi info
+  aoilats <- aoi_info[["aoilats"]]
+  aoilons <- aoi_info[["aoilons"]]
+  mask_mat <- aoi_info[["mask_mat"]]
+  resolution <- aoi_info[["resolution"]]
+
   # Get the destination file
   location <- gsub(" ", "_", tolower(parkname))
   local_park_dir <- file.path(local_dir, location)
@@ -268,26 +328,27 @@ retrieve_subset <- function(query, start_year, end_year, parkname, aoilats,
     dst_folder <- dirname(dst)
   }
 
+  # These data sets are assumed to be consistent
   if (!file.exists(dst)) {
     # Get the combined historical and modeled url query
     purl <- query[[1]]
-    
+
     # Save a local file
     ds <- xr$open_mfdataset(purl, concat_dim="time")
-    
+
     # Filter dates
     didx <- filter_years(start_year, end_year)
     ds <- ds$sel(time = c(didx[[1]]: didx[[-1]]))
-    
+
     # add coordinate
     ds <- ds$assign_coords(lat = c('lat' = as.matrix(aoilats)),
                            lon = c('lon' = as.matrix(aoilons)))
-    
+
     # Mask by boundary
     dsmask <- xr$DataArray(mask_mat)
     dsmask <- dsmask$fillna(0)
     ds <- ds$where(dsmask$data == 1)
-    
+
     # Update Attributes  <------------------------------------------------------ Standards: https://www.unidata.ucar.edu/software/netcdf-java/current/metadata/DataDiscoveryAttConvention.html
     summary = paste0(
       "This archive contains daily downscaled meteorological and hydrological ",
@@ -321,10 +382,10 @@ retrieve_subset <- function(query, start_year, end_year, parkname, aoilats,
                          "Climate Adaption Science Center"),
       "summary" = summary,
       "coordinate_system" = attrs1$coordinate_system,
-      "geospatial_lat_min" = latmin,
-      "geospatial_lat_max" = latmax,
-      "geospatial_lon_min" = lonmin,
-      "geospatial_lon_max" = lonmax,
+      "geospatial_lat_min" = min(aoilats),
+      "geospatial_lat_max" = max(aoilats),
+      "geospatial_lon_min" = min(aoilons),
+      "geospatial_lon_max" = max(aoilons),
       "geospatial_lat_units" = attrs1$geospatial_lat_units,
       "geospatial_lon_units" = attrs1$geospatial_lon_units,
       "geospatial_lat_resolution" = resolution,
@@ -359,10 +420,10 @@ retrieve_subset <- function(query, start_year, end_year, parkname, aoilats,
     )
     print("Assigning new attributes...")
     ds$attrs <- attrs2
-    
+
     # Save to local file
     ds$to_netcdf(dst)
-    
+
     # Put the file in the bucket
     if (store_remotely == TRUE) {
       object <- file.path(location, store_name)
@@ -373,10 +434,11 @@ retrieve_subset <- function(query, start_year, end_year, parkname, aoilats,
 }
 
 
+
 # Reference Classes
 Grid_Reference <- methods::setRefClass(
   "reference_grid",
-  
+
   fields = list(
     crs = "character",
     extent = "list",
@@ -386,7 +448,7 @@ Grid_Reference <- methods::setRefClass(
     ntime_hist = "numeric",
     ntime_model = "numeric"
   ),
-  
+
   methods = list(
     initialize = function(crs = paste0("+proj=longlat +a=6378137 ",
                                        "+f=0.00335281066474748 +pm=0 +no_defs"),
@@ -415,14 +477,14 @@ Grid_Reference <- methods::setRefClass(
 
 Argument_Reference <- methods::setRefClass(
   "argument_options",
-  
+
   fields = list(
     models = "character",
     parameters = "character",
     scenarios = "character",
     variables = "list",
     units = "list"),
-  
+
   methods = list(
     initialize = function(
       models = c("bcc-csm1-1", "bcc-csm1-1-m", "BNU-ESM", "CanESM2", "CCSM4",
@@ -458,22 +520,21 @@ Argument_Reference <- methods::setRefClass(
       variables <<- variables
       units <<- units
     },
-    
+
     get_args = function(model){
       args <- list()
       for (m in models){
         args[[m]] <- list("parameters" = parameters, "scenarios" = scenarios,
                           "ensemble" =  "r1i1p1")
       }
-      
+
       # Exceptions
       param_red <-  c("tasmin", "tasmax", "pr", "rsds", "uas", "vas", "huss")
       args[["NorESM1-M"]]$parameters = param_red
       args[["CCSM4"]]$parameters <- param_red
       args[["CCSM4"]]$ensemble <- "r6i1p1"
-      
+
       return(args[[model]])
     }
   )
 )
-
--- tests/testthat/test-cstdata.R
+++ tests/testthat/test-cstdata.R
@@ -1,7 +1,9 @@
 test_that("get_park_boundaries provides a shapefile", {
   skip_on_travis()
   download_dir <- tempdir()
-  get_park_boundaries(dir = download_dir)
+  aoi <-get_park_boundaries(parkname = "Yellowstone National Park",
+                            dir = download_dir)
   expected_path <- file.path(download_dir, "shapefiles", "nps_boundary.shp")
   expect_true(file.exists(expected_path))
+  expect_true(class(aoi) == "SpatialPolygonsDataFrame")
 })
