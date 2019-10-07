d8aed162be2f9a6079b36502a7c907a62b4c8652
williamstravis <travis.williams@colorado.edu>
2019-10-07 09:50:23 -0600

    A few changes to the test script

--- tests/testthat/test-cstdata.R
+++ tests/testthat/test-cstdata.R
@@ -1,4 +1,4 @@
-test_that("get_park_boundaries provides a shapefile", {
+test_that("get_park_boundaries provides a shapefile and spatial polygon", {
   skip_on_travis()
   download_dir <- tempdir()
   aoi <-get_park_boundaries(parkname = "Yellowstone National Park",
@@ -7,3 +7,22 @@ test_that("get_park_boundaries provides a shapefile", {
   expect_true(file.exists(expected_path))
   expect_true(class(aoi) == "SpatialPolygonsDataFrame")
 })
+
+# test_that("Test that config_aws properly stores configuration file", {
+#   skip_on_travis()
+#   fake_bucket <- "fake_bucket"
+#   fake_key <- "a;dfhlgjhsd"
+#   fake_skey <- "a;hjslkdjghsdkjgh"
+#   region <- "us-west-2"
+#   # how do we test user input?
+# })
+
+# filter years
+
+# get_aoi_indexes
+
+# get_aoi_info
+
+# get_grouped _queries
+
+# retrieve_subset
