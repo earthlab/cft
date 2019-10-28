c9705f5bbd3323ec46f5665a3ed0d5a728525c91
williamstravis <travis.williams@colorado.edu>
2019-10-27 21:52:21 -0600

    Windows required special reticulation. So far it will only work with conda with windows, though there should be a way around this

--- R/reticulate_setup.R
+++ R/reticulate_setup.R
@@ -1,9 +1,16 @@
 # We need these for xarray to work properly
 deps <- c("dask", "netcdf4", "toolz", "xarray")
 
-# If needed install these to the default reticualte virtual env
-for (dep in deps) {
+# If needed install these to the default reticulate virtual env
+if (!Sys.info()['sysname'] == "Windows"){
   if (!reticulate::py_module_available(dep)) {
-    reticulate::py_install(dep, method = "auto", conda = "auto")
+    for (dep in deps) {
+      # Conda not required here
+      reticulate::py_install(dep, method = "auto", conda = "auto")
+    }
   }
+} else {
+    # Surely, conda isn't absolutely necessary for windows?
+    reticulate::conda_install(envname = "r-reticulate",
+                              packages = deps)
 }
