6155d84cb53a94c2b07a1f3fc3c488a4d537b5b7
Max Joseph <maxwellbjoseph@gmail.com>
2020-01-28 15:35:09 -0700

    Add a function to get a data frame summarizing climate data

---
+++ tests/testthat/test-cst_df.R
@@ -0,0 +1,11 @@
+test_that("cst_df generates a tibble", {
+  local_dir <- "~" #tempdir()
+  file_refs <- cstdata(park = "Acadia National Park",
+                       years = c(2004, 2005),
+                       models = c("bcc-csm1-1"),
+                       parameters = c("pr"),
+                       scenarios = c("rcp45"),
+                       local_dir = local_dir)
+  out <- cst_df(file_reference = file_refs)
+  expect_s3_class(out, "data.frame")
+})
