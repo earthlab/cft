54f11ce5de145dd57dc776a407ee8a06451797a2
Max Joseph <maxwellbjoseph@gmail.com>
2020-04-16 10:43:48 -0600

    Vignette improvement with precip (#116)
    * Vignette improvement with pr
    
    * Add title in yaml

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -2,7 +2,7 @@
 # Precompiled vignettes that are long-running (requiring data downloads)
 
 library(knitr)
-knit("vignettes/cst-intro.Rmd.orig", "vignettes/cst-intro.Rmd")
+knit("vignettes/cft-intro.Rmd.orig", "vignettes/cft-intro.Rmd")
 figs <- list.files(pattern = ".png")
 file.copy(figs, "vignettes/", overwrite = TRUE)
 unlink(figs)
