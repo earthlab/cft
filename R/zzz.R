57c92d2ff73e0833e690faac9caa29b7e09c0589
Max Joseph <maxwellbjoseph@gmail.com>
2020-07-06 14:14:46 -0600

    update nps boundary url

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 nps_boundary_url <- function() {
   # return default nps boundary url
-  "https://irma.nps.gov/DataStore/DownloadFile/637623"
+  "https://irma.nps.gov/DataStore/DownloadFile/643238"
 }
 
