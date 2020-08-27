c0bda56c29d62da97415a617bb75cde184f2f9fa
Max Joseph <maxwellbjoseph@gmail.com>
2020-08-27 08:41:54 -0600

    Add support and tests for point AOIs

--- tests/testthat/test-cftdata.R
+++ tests/testthat/test-cftdata.R
@@ -21,3 +21,18 @@ test_that("A cftdata run on a one pixel park completes successfully", {
                        scenarios = "rcp85")
   expect_true(file.exists(file_refs$local_path))
 })
+
+test_that("A cftdata run with a point aoi works", {
+  pt <- sp::SpatialPointsDataFrame(
+    coords = data.frame(lon = -77, lat = 39), 
+    data = data.frame(id = 1), 
+    proj4string = sp::CRS("+proj=longlat +datum=WGS84"))
+
+  file_refs <- cftdata(aoi = pt, 
+                       area_name = "wolftrap",
+                       parameters = "tasmax",
+                       years = c(2000, 2001), 
+                       models = "CCSM4", 
+                       scenarios = "rcp85")
+  expect_true(file.exists(file_refs$local_path))
+})
