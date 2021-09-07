8d87436270323d626807e94bd43bc995e6595a95
Ty Tuff <ty.tuff@mail.mcgill.ca>
2021-09-07 14:03:51 -0600

    intro update

--- R/subsetting.R
+++ R/subsetting.R
@@ -81,6 +81,7 @@ get_aoi_info <- function(aoi, grid_ref) {
     orig_crs <- raster::projection(aoi)
     # buffering is only possible in projected coordinate systems
     proj_aoi <- sp::spTransform(aoi, raster::crs("+init=epsg:5070"))
+    #proj_aoi <- sp::spTransform(aoi, raster::crs("+proj=longlat +datum=WGS84 +init=epsg:4326"))
     aoi <- rgeos::gBuffer(proj_aoi, width=.1) #why 0.1? what units?
     aoi <- sp::spTransform(aoi, raster::crs(orig_crs))
   }
@@ -306,7 +307,7 @@ retrieve_subset <- function(query, years, aoi_info, area_name, local_dir) {
     
     ndate <- length(unlist(query$dates))
     if (any_historical & any_future) {
-      #stopifnot(dim(historical_array)[1:2] == dim(future_array)[1:2])
+      stopifnot(dim(historical_array)[1:2] == dim(future_array)[1:2])
       stopifnot(ndate == (dim(historical_array)[3] + dim(future_array)[3]))
       final_array <- array(c(historical_array, future_array), 
                          dim = c(dim(future_array)[1:2], 
