512571b651b2ed5fd6a3c6b91c58235797aefddb
meco6443 <meco6443@colorado.edu>
2022-08-22 13:37:55 -0600

    Updated the plot_cubes_3d function

--- R/plot_cubes_3d.R
+++ R/plot_cubes_3d.R
@@ -13,37 +13,36 @@
 plot_cubes_3d <- function(cube1, cube2){
   require(rgl)
   
-  # define a vector of possible names for the latitude and longitude data columns in each cube
+  # define a vector of possible names for the latitude, longitude, and time data columns in each cube
   ynames <- c('lat', 'latitude', 'Lat', 'Latitude', 'y')
   xnames <- c('lon', 'long', 'longitude', 'Lon', 'Long', 'Longitude', 'x')
+  tnames <- c('time', 'day', 'hour', 'month', 'year', 'date')
 
-  # find the indices of the columns containing longitude and latitude data in cube 1, respectively
+  # find the indices of the columns containing longitude, latitude, and time data in cube 1, respectively
   c1_xname <- which(colnames(cube1) %in% xnames)
   c1_yname <- which(colnames(cube1) %in% ynames)
+  c1_tname <- which(colnames(cube1) %in% tnames)
 
-  # find the indices of the columns containing longitude and latitude data in cube 2, respectively
+  # find the indices of the columns containing longitude, latitude, and time data in cube 2, respectively
   c2_xname <- which(colnames(cube2) %in% xnames)
   c2_yname <- which(colnames(cube2) %in% ynames)
+  c2_tname <- which(colnames(cube2) %in% tnames)
   
-  # find the longitudinal and latitudinal resolution of the data in cube 1
-  c1_xres <- resolution(cube1[,c1_xname])
-  c1_yres <- resolution(cube1[,c1_yname])
-  
-  # find the longitudinal and latitudinal resolution of the data in cube 2
-  c2_xres <- resolution(cube2[,c2_xname])
-  c2_yres <- resolution(cube2[,c2_yname])
-  
-  # find the minimum and maximum longitude and latitude values in cube 1
+  # find the minimum and maximum longitude, latitude, and time values in cube 1
   c1_xmin <- min(cube1[,c1_xname])
   c1_xmax <- max(cube1[,c1_xname])
   c1_ymin <- min(cube1[,c1_yname])
   c1_ymax <- max(cube1[,c1_yname])
+  c1_tmin <- min(cube1[,c1_tname])
+  c1_tmax <- max(cube1[,c1_tname])
   
-  # find the minimum and maximum longitude and latitude values in cube 2
+  # find the minimum and maximum longitude, latitude, and time values in cube 2
   c2_xmin <- min(cube2[,c2_xname])
   c2_xmax <- max(cube2[,c2_xname])
   c2_ymin <- min(cube2[,c2_yname])
   c2_ymax <- max(cube2[,c2_yname])
+  c2_tmin <- min(cube2[,c2_tname])
+  c2_tmax <- max(cube2[,c2_tname])
   
   # set RGL options so the 3D plot appears and has a white background to make it easier to see the cubes
   options(rgl.printRglwidget = TRUE)
@@ -52,12 +51,12 @@ plot_cubes_3d <- function(cube1, cube2){
   # create a 3D cube plot for cube 1 by first creating a unit cube that is medium slate blue in color and fairly transparent
   c3d <- cube3d(color="mediumslateblue", alpha=0.3)
   
-  # create vectors that define the indices of cube 1
+  # create vectors that define the vertices of cube 1
   c1x = c(c1_xmin, c1_xmax, c1_xmin, c1_xmax, c1_xmin, c1_xmax, c1_xmin, c1_xmax)
   c1y = c(c1_ymin, c1_ymin, c1_ymax, c1_ymax, c1_ymin, c1_ymin, c1_ymax, c1_ymax)
   c1z = c(c1_tmin, c1_tmin, c1_tmin, c1_tmin, c1_tmax, c1_tmax, c1_tmax, c1_tmax)
 
-  # change the indices of the 3D unit cube to match the indices of cube 1
+  # change the vertices of the 3D unit cube to match the vertices of cube 1
   c3d$vb[1,] <- c1x
   c3d$vb[2,] <- c1y
   c3d$vb[3,] <- c1z
@@ -65,12 +64,12 @@ plot_cubes_3d <- function(cube1, cube2){
   # create a 3D cube plot for cube 2 by first creating a unit cube that is medium spring green in color and fairly transparent
   c3d2 <- cube3d(color="mediumspringgreen", alpha=0.3)
   
-  # create vectors that define the indices of cube 2
+  # create vectors that define the vertices of cube 2
   c2x = c(c2_xmin, c2_xmax, c2_xmin, c2_xmax, c2_xmin, c2_xmax, c2_xmin, c2_xmax)
   c2y = c(c2_ymin, c2_ymin, c2_ymax, c2_ymax, c2_ymin, c2_ymin, c2_ymax, c2_ymax)
   c2z = c(c2_tmin, c2_tmin, c2_tmin, c2_tmin, c2_tmax, c2_tmax, c2_tmax, c2_tmax)
 
-  # change the indices of the 3D unit cube ot match the indices of cube 2
+  # change the vertices of the 3D unit cube to match the vertices of cube 2
   c3d2$vb[1,] <- c2x
   c3d2$vb[2,] <- c2y
   c3d2$vb[3,] <- c2z
